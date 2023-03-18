21f9b64441fa422a8be5f1541eefa4bb71a9aad6
Jack Torn <j4cktorn@gmail.com>
2023-03-18 16:57:19 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jack Torn on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
