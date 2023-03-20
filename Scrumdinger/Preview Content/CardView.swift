ef6b0f2c925260e3deb4ff4db284ee56f2a3de94
Jack Torn <j4cktorn@gmail.com>
2023-03-20 12:18:59 +0000

    section 3 completed

---
+++ Scrumdinger/Preview Content/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Jack Torn on 20/03/2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
